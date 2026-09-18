6ff2ee8677f3af2577c6db6da2b797efa67f5868
hugo-bluecorn <15798087+hugo-bluecorn@users.noreply.github.com>
2026-09-18 15:13:10 +0200

    Pin Flutter 3.47.2 and create sensorctl

---
+++ apps/sensorctl/bin/sensorctl.dart
@@ -0,0 +1,5 @@
+import 'package:sensorctl/sensorctl.dart' as sensorctl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sensorctl.calculate()}!');
+}
---
+++ apps/sensorctl/test/sensorctl_test.dart
@@ -0,0 +1,8 @@
+import 'package:sensorctl/sensorctl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
